13f542f12cfa9da5ac4b151e9526db3ff7c7b6d0
Imad Selka <imadselkawi133@gmail.com>
2023-12-28 19:16:07 +0100

    last changes

---
+++ app/components/main/index.js
@@ -0,0 +1,7 @@
+export { default as Encryption } from "./Encryption/Encryption";
+export { default as Footer } from "./Footer/Footer";
+export { default as Hero } from "./Hero/Hero";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Projects } from "./Projects/Projects";
+export { default as Skills } from "./Skills/Skills";
+export { default as StarsCanvas } from "./StarBackground/StarBackground";
---
+++ app/components/sub/index.js
@@ -0,0 +1,4 @@
+export { default as HeroContent } from "./HeroContent/HeroContent";
+export { default as ProjectCard } from "./ProjectCard/ProjectCard";
+export { default as SkillDataProvider } from "./SkillDataProvider/SkillDataProvider";
+export { default as SkillText } from "./SkillText/SkillText";
